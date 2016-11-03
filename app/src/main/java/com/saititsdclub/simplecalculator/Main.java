99dfc85bece972f6ea258500060299d53c30b471
little-apps <nick@little-apps.com>
2016-11-03 08:19:00 -0600

    Added Android Studio project

---
+++ app/src/main/java/com/saititsdclub/simplecalculator/Main.java
@@ -0,0 +1,13 @@
+package com.saititsdclub.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+}
