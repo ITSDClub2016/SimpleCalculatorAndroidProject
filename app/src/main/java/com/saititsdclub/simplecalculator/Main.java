f675238053daceb1641805ab3c7c0848a8234554
little-apps <nick@little-apps.com>
2016-12-18 18:34:38 -0700

    Implemented user interface

--- app/src/main/java/com/saititsdclub/simplecalculator/Main.java
+++ app/src/main/java/com/saititsdclub/simplecalculator/Main.java
@@ -2,12 +2,28 @@ package com.saititsdclub.simplecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
 public class Main extends AppCompatActivity {
+    EditText editTextFormula;
+    EditText editTextAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+
+        editTextFormula = (EditText)findViewById(R.id.editTextFormula);
+        editTextAnswer = (EditText)findViewById(R.id.editTextAnswer);
+
+        this.setEditTextOnlySelectable(editTextFormula);
+        this.setEditTextOnlySelectable(editTextAnswer);
+    }
+
+    private void setEditTextOnlySelectable(EditText editText) {
+        editText.setInputType(InputType.TYPE_NULL);
+        editText.setTextIsSelectable(true);
+        editText.setKeyListener(null);
     }
 }
